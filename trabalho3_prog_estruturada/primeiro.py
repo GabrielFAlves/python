d3a0bc8cba5363427ae45dbae882059fab3e14ac
PAULO HENRIQUE COSTA PEREIRA NUNES <202303378785@alunos.ibmec.edu.br>
2023-06-06 16:18:01 -0300

    paulo

--- trabalho3_prog_estruturada/primeiro.py
+++ trabalho3_prog_estruturada/primeiro.py
@@ -1,59 +1,50 @@
-def is_leap_year(year):
+def ano_bissexto(ano):
     """Verifica se um ano é bissexto."""
-    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+    return ano % 4 == 0 and (ano % 100 != 0 or ano % 400 == 0)
 
-def days_in_month(year, month):
+def dia_mes(ano, mes):
     """Retorna o número de dias em um determinado mês."""
-    if month == 2:
-        return 29 if is_leap_year(year) else 28
-    elif month in [4, 6, 9, 11]:
+    if mes == 2:
+        return 29 if ano_bissexto(ano) else 28
+    elif mes in [4, 6, 9, 11]:
         return 30
     else:
         return 31
 
-def date_difference(start_date, end_date):
+def diferenca_data(data_inicio, data_fim):
     """Calcula o número de dias entre duas datas."""
-    start_year, start_month, start_day = start_date
-    end_year, end_month, end_day = end_date
+    ano_inicio, mes_inicio, dia_inicio = data_inicio
+    ano_fim, mes_fim, dia_fim = data_fim
 
-    days = 0
+    dias = 0
 
-    while start_date != end_date:
-        days += 1
-        start_day += 1
+    while data_inicio != data_fim:
+        dias += 1
+        dia_inicio += 1
 
-        if start_day > days_in_month(start_year, start_month):
-            start_day = 1
-            start_month += 1
+        if dia_inicio > dia_mes(ano_inicio, mes_inicio):
+            dia_inicio = 1
+            mes_inicio += 1
 
-            if start_month > 12:
-                start_month = 1
-                start_year += 1
+            if mes_inicio > 12:
+                mes_inicio = 1
+                ano_inicio += 1
 
-        start_date = (start_year, start_month, start_day)
+        data_inicio = (ano_inicio, mes_inicio, dia_inicio)
 
-    return days
+    return dias
 
 # Função principal
-def main():
-    while True:
-        start_year = int(input("Digite o ano da primeira data (4 dígitos): "))
-        if start_year == 0:
-            break
+ano_inicio = int(input("Digite o ano da primeira data (4 dígitos): "))
+mes_inicio = int(input("Digite o mês da primeira data: "))
+dia_inicio = int(input("Digite o dia da primeira data: "))
 
-        start_month = int(input("Digite o mês da primeira data: "))
-        start_day = int(input("Digite o dia da primeira data: "))
+ano_fim = int(input("Digite o ano da segunda data (4 dígitos): "))
+mes_fim = int(input("Digite o mês da segunda data: "))
+dia_fim = int(input("Digite o dia da segunda data: "))
 
-        end_year = int(input("Digite o ano da segunda data (4 dígitos): "))
-        end_month = int(input("Digite o mês da segunda data: "))
-        end_day = int(input("Digite o dia da segunda data: "))
+data_inicio = (ano_inicio, mes_inicio, dia_inicio)
+data_fim = (ano_fim, mes_fim, dia_fim)
 
-        start_date = (start_year, start_month, start_day)
-        end_date = (end_year, end_month, end_day)
-
-        days = date_difference(start_date, end_date)
-        print("Número de dias corridos:", days)
-        print()
-
-if __name__ == "__main__":
-    main()
+dias = diferenca_data(data_inicio, data_fim)
+print("Número de dias corridos:", dias)
